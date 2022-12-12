267692e882e13795037d79bbce6e1e06ca27b43a
Pedro Oliveira <22790804+JPedro2@users.noreply.github.com>
2022-12-12 23:11:07 +0000

    changing header

--- pages/index.js
+++ pages/index.js
@@ -67,8 +67,7 @@ export default function Form({ applianceId }) {
         )}
       </div>
       <h1 className={styles.title}>
-       //<span className="accent">{name}</span> Edge Onboarding - Host Registration
-        Edge Onboarding - Host Registration
+       <span className="accent">{name}</span> Edge Onboarding - Host Registration
       </h1>
       <p className={styles.description}>
         Register the Edge Host with Palette
